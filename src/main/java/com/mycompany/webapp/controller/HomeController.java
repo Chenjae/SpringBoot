cbc879bdf2a276ce95d06fe6bd8cdb79f86763b8
Chenjae <injea95@naver.com>
2021-10-14 18:01:35 +0900

    21-10-14 수업내용

---
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.mycompany.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		log.error("실행");
+		log.warn("실행");
+		log.info("실행");
+		log.debug("실행");
+		return "home";
+	}
+}
